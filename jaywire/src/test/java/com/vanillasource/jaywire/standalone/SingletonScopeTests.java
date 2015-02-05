bc62216549282ebddd86855f494ded533587289a
Robert Brautigam <robert.brautigam.gyula@gmail.com>
2015-02-05 18:03:40 +0100

    Added tests for singleton scope

---
+++ jaywire/src/test/java/com/vanillasource/jaywire/standalone/SingletonScopeTests.java
@@ -0,0 +1,41 @@
+/**
+  * Copyright (C) 2015 VanillaSource
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 3 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+
+package com.vanillasource.jaywire.standalone;
+
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+import java.util.function.Supplier;
+
+@Test
+public class SingletonScopeTests {
+
+   @SuppressWarnings("unchecked")
+   public void testSupplierUsedOnlyOnce() {
+      SingletonScope scope = new SingletonScope();
+      Supplier<Object> supplier = mock(Supplier.class);
+
+      Object result1 = scope.get(supplier);
+      Object result2 = scope.get(supplier);
+
+      assertSame(result1, result2);
+   }
+}
+
+
