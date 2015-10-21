43b7e7994ac9e77d8509468a70ec7ff3400be82a
Robert Braeutigam <brautigam.robert.gyula@gmail.com>
2015-10-21 13:50:41 +0200

    Added session and request scope to ee integration

---
+++ jaywire-ee/src/main/java/com/vanillasource/jaywire/ee/CdiSessionScope.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2015 VanillaSource
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.vanillasource.jaywire.ee;
+
+import javax.enterprise.context.SessionScoped;
+import com.vanillasource.jaywire.standalone.SingletonScope;
+import java.io.Serializable;
+
+@SessionScoped
+public class CdiSessionScope extends SingletonScope implements Serializable {
+}
+
