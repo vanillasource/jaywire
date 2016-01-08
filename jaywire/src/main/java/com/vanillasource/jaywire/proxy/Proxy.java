17a547377504153a3f1c338217423ec841794973
Robert Brautigam <robert.brautigam.gyula@gmail.com>
2016-01-08 14:00:18 +0100

    Added proxy interceptor definitions without T

--- jaywire/src/main/java/com/vanillasource/jaywire/proxy/Proxy.java
+++ jaywire/src/main/java/com/vanillasource/jaywire/proxy/Proxy.java
@@ -19,11 +19,66 @@
 package com.vanillasource.jaywire.proxy;
 
 import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Proxies calls to an unerlying object.
  */
 @FunctionalInterface
 public interface Proxy<T> {
+   /**
+    * Call object inside this proxy. The object will
+    * be supplied to the function given.
+    */
    <R> R call(Function<T, R> body);
+
+   /**
+    * Execute additional logic around the call. To execute
+    * the body supplied by user, call the received runnable.
+    * If the runnable is not called in the 'around' handler,
+    * null will be returned.
+    * @return A proxy that will execute the supplied 'around'
+    * handler.
+    */
+   default Proxy<T> onAround(Consumer<Runnable> around) {
+      return new Proxy<T>() {
+         @Override
+         public <R> R call(Function<T, R> body) {
+            List<R> result = new ArrayList<>(1);
+            around.accept(new Runnable() {
+               @Override
+               public void run() {
+                  result.add(call(body));
+               }
+            });
+            if (result.isEmpty()) {
+               return null;
+            }
+            return result.get(0);
+         }
+      };
+   }
+
+   /**
+    * Execute a certain logic before the user supplied body is run.
+    */
+   default Proxy<T> onBefore(Runnable before) {
+      return onAround(body -> {
+         before.run();
+         body.run();
+      });
+   }
+
+   /**
+    * Execute a certain logic after the user supplied body is run.
+    */
+   default Proxy<T> onAfter(Runnable after) {
+      return onAround(body -> {
+         body.run();
+         after.run();
+      });
+   }
 }
+
